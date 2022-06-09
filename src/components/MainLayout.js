29f9a3dcaa9803e867d5c72adb22ba0dab929e8e
Julian Rojas <julian.rojas87@gmail.com>
2022-06-09 09:18:13 +0200

    Error handling and minor style changes

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -76,15 +76,22 @@ class MainLayout extends Component {
     };
 
     readLDES = async () => {
-        const res = await fetch(`http://localhost:8080/rinf-ldes/${this.state.country}`);
-        const data = await res.json();
-        const members = data.members.map(d => {
-            d['dct:isVersionOf'] = BASE_URI + d['dct:isVersionOf'].split('era:')[1];
-            if(d['era:gaugingProfile']) d['era:gaugingProfile'] = d['era:gaugingProfile']['@id'];
-            return d;
-        }).reverse();
-        console.log(members)
-        this.setState({ data: [...members] });
+        try {
+            const res = await fetch(`http://localhost:8080/rinf-ldes/${this.state.country}`);
+            const data = await res.json();
+            const members = data.members.map(d => {
+                d['dct:isVersionOf'] = BASE_URI + d['dct:isVersionOf'].split('era:')[1];
+                if (d['era:gaugingProfile']) {
+                    d['era:gaugingProfile'] = BASE_URI + d['era:gaugingProfile']['@id'].split('era:')[1];
+                };
+                return d;
+            }).reverse();
+            console.log(members)
+            this.setState({ data: [...members] });
+        } catch(err) {
+            console.log(`Not data found for ${this.state.country}`);
+            this.setState({ data: [] });
+        }
     }
 
     render() {
@@ -92,33 +99,37 @@ class MainLayout extends Component {
         return (
             <div className="show-fake-browser sidebar-page">
                 <Container>
-                <Table height={800} data={data}>
-                    <Table.Column width={700} align="center" fixed>
-                        <Table.HeaderCell>URI</Table.HeaderCell>
-                        <Table.Cell dataKey="dct:isVersionOf" />
-                    </Table.Column>
-                    <Table.Column width={100} align="center" fixed>
-                        <Table.HeaderCell><a href="http://data.europa.eu/949/trackId">Track ID</a></Table.HeaderCell>
-                        <Table.Cell dataKey="era:trackId" />
-                    </Table.Column>
-                    <Table.Column width={200} align="center" fixed>
-                        <Table.HeaderCell><a href="http://data.europa.eu/949/maximumPermittedSpeed">Maximum permitted speed</a></Table.HeaderCell>
-                        <Table.Cell dataKey="era:maximumPermittedSpeed" />
-                    </Table.Column>
-                    <Table.Column width={300} align="center" fixed>
-                        <Table.HeaderCell><a href="http://data.europa.eu/949/gaugingProfile">Gauging Profile</a></Table.HeaderCell>
-                        <Table.Cell dataKey="era:gaugingProfile" />
-                    </Table.Column>
-                    <Table.Column width={300} align="center" fixed>
-                        <Table.HeaderCell><a href="http://data.europa.eu/949/cantDeficiency">Cant Deficiency</a></Table.HeaderCell>
-                        <Table.Cell dataKey="era:cantDeficiency" />
-                    </Table.Column>
-                    <Table.Column width={300} align="center" fixed>
-                        <Table.HeaderCell>Last Modified</Table.HeaderCell>
-                        <Table.Cell dataKey="dct:modified" />
-                    </Table.Column>
-                </Table>
-            </Container>
+                    <Table height={800} data={data}>
+                        <Table.Column width={700} align="center" fixed>
+                            <Table.HeaderCell>URI</Table.HeaderCell>
+                            <Table.Cell>
+                             {rowData => <a href={rowData['dct:isVersionOf']}>{rowData['dct:isVersionOf']}</a>}
+                            </Table.Cell>
+                        </Table.Column>
+                        <Table.Column width={100} align="center" fixed>
+                            <Table.HeaderCell><a href="http://data.europa.eu/949/trackId">Track ID</a></Table.HeaderCell>
+                            <Table.Cell dataKey="era:trackId" />
+                        </Table.Column>
+                        <Table.Column width={200} align="center" fixed>
+                            <Table.HeaderCell><a href="http://data.europa.eu/949/maximumPermittedSpeed">Maximum permitted speed</a></Table.HeaderCell>
+                            <Table.Cell dataKey="era:maximumPermittedSpeed" />
+                        </Table.Column>
+                        <Table.Column width={400} align="center" fixed>
+                            <Table.HeaderCell><a href="http://data.europa.eu/949/gaugingProfile">Gauging Profile</a></Table.HeaderCell>
+                            <Table.Cell>
+                                {rowData => <a href={rowData['era:gaugingProfile']}>{rowData['era:gaugingProfile']}</a>}
+                            </Table.Cell>
+                        </Table.Column>
+                        <Table.Column width={200} align="center" fixed>
+                            <Table.HeaderCell><a href="http://data.europa.eu/949/cantDeficiency">Cant Deficiency</a></Table.HeaderCell>
+                            <Table.Cell dataKey="era:cantDeficiency" />
+                        </Table.Column>
+                        <Table.Column width={300} align="center" fixed>
+                            <Table.HeaderCell>Last Modified</Table.HeaderCell>
+                            <Table.Cell dataKey="dct:modified" />
+                        </Table.Column>
+                    </Table>
+                </Container>
             </div>
         );
     }
